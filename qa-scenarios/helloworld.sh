6e0ab922ea2eb2a19a024247a353947690f9344e
jeremyletang <letang.jeremy@gmail.com>
2019-10-15 12:14:00 +0100

    update qa-attempt to run scripts

--- qa-scenarios/helloworld.sh
+++ qa-scenarios/helloworld.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-clear
-killall tendermint
-killall vega
-
-echo "Waiting 1 sec"
-sleep 1
-
-# This assumes you have tendermint running locally: 
-tendermint unsafe_reset_all && tendermint init && tendermint node  2> ./tendermint.stderr.out 1> ./tenderming.stdout.out &
-# and fresh Vega: 
-rm -rf "$HOME/.vega/"*store && vega node 2> ./vega.stderr.out 1> ./vega.stdout.out &
-
-echo "Waiting 5 sec"
-sleep 5
-
 # and that you've created accounts for david and edd:
+echo "creating accounts"
 vegaccount -traderid edd
 vegaccount -traderid david
 echo "Waiting 1 sec" && sleep 1
+
 
 # add some orders the party david is "market making" this creates volume on the book
 curl 'http://localhost:3004/query' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Connection: keep-alive' -H 'DNT: 1' -H 'Origin: http://localhost:3004' --data-binary '{"query":"mutation {\n  orderSubmit(\n    marketId: \"SM5FP5KTOKZHBPP5Q7U7WJ7BWH2J4IUL\"\n    partyId:\"david\"\n    price: \"10100\"\n    size: \"1\"\n    side:Sell\n    timeInForce:GTC\n  ) {\n    reference\n  }\n}"}' --compressed
@@ -33,6 +20,7 @@ curl 'http://localhost:3004/query' -H 'Accept-Encoding: gzip, deflate, br' -H 'C
 
 curl 'http://localhost:3004/query' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Connection: keep-alive' -H 'DNT: 1' -H 'Origin: http://localhost:3004' --data-binary '{"query":"mutation {\n  orderSubmit(\n    marketId: \"SM5FP5KTOKZHBPP5Q7U7WJ7BWH2J4IUL\"\n    partyId:\"david\"\n    price: \"9700\"\n    size: \"10\"\n    side:Buy\n    timeInForce:GTC\n  ) {\n    reference\n  }\n}"}' --compressed
 
+
 echo ""
 echo "Waiting 2 sec" && sleep 2
 
@@ -42,23 +30,10 @@ curl 'http://localhost:3004/query' -H 'Accept-Encoding: gzip, deflate, br' -H 'C
 
 
 echo ""
-echo "Waiting 1 sec" && sleep 1 
+echo "Waiting 1 sec" && sleep 1
 
 # Let's see open orders
 curl 'http://localhost:3004/query' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Connection: keep-alive' -H 'DNT: 1' -H 'Origin: http://localhost:3004' --data-binary '{"query":"query {\n  markets(id:\"SM5FP5KTOKZHBPP5Q7U7WJ7BWH2J4IUL\") {\n    id\n    name\n    orders(last: 20) {\n      id\n      party { id }\n      price\n      side\n      size\n      remaining\n    }\n  }\n}\n"}' --compressed
 
 
 echo ""
-
-# git it a bit of time and then kill stuff
-#sleep 1
-#killall tendermint
-#killall vega
-
-
-
-
-
-
-
-
---
+++ qa-scenarios/runner.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# require 1 argument = the test file to run
+if test "$#" -lt 1 ; then
+    echo "error: need test file"
+    echo "usage: ./runner.sh [testname]"
+    exit 0
+fi
+
+# if file does not exists, exit with error
+if [ ! -f "$1.sh" ]; then
+    echo "error: file not found ($1.sh)"
+    exit 0
+fi
+
+testname=$1
+testfile="$1.sh"
+
+clear
+killall tendermint
+killall vega
+
+echo "Waiting 1 sec"
+sleep 1
+
+# This assumes you have tendermint running locally:
+tendermint unsafe_reset_all && tendermint init && tendermint node  2> ./tendermint.stderr.out 1> ./tendermint.stdout.out &
+# and fresh Vega:
+rm -rf "$HOME/.vega/"*store && vega node 2> ./vega.stderr.out 1> ./vega.stdout.out &
+
+echo "Waiting 5 sec"
+sleep 5
+
+# starting the streaming stuff
+echo "starting vega streams"
+vegastream -orders 2>> "$testfile.orders.out" 1>> "$testfile.orders.out" &
+vegastream -trades 2>> "$testfile.trades.out" 1>> "$testfile.trades.out" &
+vegastream -accounts 2>> "$testfile.accounts.out" 1>> "$testfile.accounts.out" &
+vegastream -transfers  2>> "$testfile.transfers.out" 1>> "$testfile.transfers.out"&
+vegastream -trades 2>> "$testfile.trades.out" 1>> "$testfile.trades.out" &
+
+echo -e "executing testfile: $testfile"
+./$testfile
+echo "cleaning up"
+
+# get it a bit of time and then kill stuff
+sleep 1
+killall vegastream
+killall tendermint
+killall vega
